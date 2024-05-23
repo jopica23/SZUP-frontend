3102710349979f6bcdc348f7128874ad8ca90df9
jopica23 <126028293+jopica23@users.noreply.github.com>
2024-05-23 11:04:26 +0200

    blablaa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
+//komentar
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
